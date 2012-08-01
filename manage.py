9356aae101921de8adbb44306bd6a41e0d05f72e
Laine <laine.herron@10gen.com>
2012-08-01 10:49:41 -0700

    cleaned some things up

--- manage.py
+++ manage.py
@@ -8,60 +8,41 @@ import routings
 
 
 def make_initial_posts():
-    user1 = models.User(username="user1", password="password")
+    user1 = models.User(username='user1', password='password')
     user1.save()
-    user2 = models.User(username="user2", password="password")
+    user2 = models.User(username='user2', password='password')
     user2.save()
-    comment1 = models.Comment(
-                    author=user1,
-                    content="I have something to say about that"
-                    )
-    comment2 = models.Comment(
-                    author=user2,
-                    content="Ready my comment!"
-                    )
-    comment3 = models.Comment(
-                    author=user2,
-                    content="yes you an comment on your own psot"
-                    )
-    post1 = models.Post(
-                author=user1,
-                title = "The First Post",
-                content = "Yay, I'm first!",
-                comments = [comment1, comment2]
-                )
-    post1.save()
-    post2 = models.Post(
-                author=user2,
-                title = "The second post is still important",
-                content = "Just saying",
-                comments = [comment3]
-                )
-    post2.save()
+    comment = models.Comment(author=user2, content='Ready my comment!')
+    post = models.Post(author=user1, title='The First Post',
+                        content="Yay, I'm first!", comments=[comment])
+    post.save()
 
 
+# Specify and get command line arguments
 
 parser = argparse.ArgumentParser()
-parser.add_argument("command", type=str,
+parser.add_argument('command', type=str,
                     help="specify 'runserver' to start the server")
-parser.add_argument("-host", type=str,
-                    help="specify server host, defaults to localhost")
-parser.add_argument("-port", type=int,
-                    help="specify server port, defaults to 8080")
+parser.add_argument('-host', type=str,
+                    help='specify server host, defaults to localhost')
+parser.add_argument('-port', type=int,
+                    help='specify server port, defaults to 8080')
+parser.add_argument('-mongo_host', type=str,
+                    help='specify MongoDB host, defaults to localhost')
+parser.add_argument('-mongo_port', type=int,
+                    help='specify MongoDB port, defaults to 27017')
 args = parser.parse_args()
 
-if args.host:
-    host = args.host
-else:
-    host = 'localhost'
+# Use command line arguments / run servers
 
-if args.port:
-    port = args.port
-else:
-    port = 8080
+host = (args.host if args.host else 'localhost')
+port = (args.port if args.port else 8080)
+mongo_host = (args.mongo_host if args.mongo_host else 'localhost')
+mongo_port = (args.mongo_port if args.mongo_port else 27017)
 
-if args.command == "runserver":
-    mongoengine.connect('mytumblelog(%s)'%str(random.randint(1,9999)))
+if args.command == 'runserver':
+    mongoengine.connect('mytumblelog(%s)' % str(random.randint(1,
+                        9999)))
     make_initial_posts()
     run(host='localhost', port=8080)
-    
+
--- routings.py
+++ routings.py
@@ -1,69 +1,79 @@
 from bottle import route, post, redirect, template, request
 import models
 
+
 @route('/')
 def main_page():
-    return template(
-            'templates/main.html',
-            current_user = models.current_user,
-            posts=models.Post.objects,
-            show_post=False,
-            show_comment=False,
-           )
+    return template('templates/main.html',
+                    current_user=models.current_user,
+                    posts=models.Post.objects, show_post=False,
+                    show_comment=False)
+
 
 @route('/sign_in')
 def sign_in_page(message=None):
-    return template(
-            'templates/sign_in.html',
-            current_user = models.current_user, 
-            message = message
-           )
+    return template('templates/sign_in.html',
+                    current_user=models.current_user, message=message)
+
 
 @post('/sign_in')
 def sign_in_action():
-    if request.forms.get("button") == "Sign in":
-        username = request.forms.get("username")
-        password = request.forms.get("password")
+
+    # Get fields from HTML form
+
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    new_username = request.forms.get('new_username')
+    new_password1 = request.forms.get('new_password1')
+    new_password2 = request.forms.get('new_password2')
+
+    # Do logic for the forms
+
+    if request.forms.get('button') == 'Sign in':
         if models.User.objects(username=username).count() == 0:
-            return sign_in_page(message="username not found")
+            return sign_in_page(message='username not found')
         else:
-            linked_author = models.User.objects(username=username).first()
+            linked_author = \
+                models.User.objects(username=username).first()
             if not password == linked_author.password:
-                return sign_in_page(message="incorrect password")
+                return sign_in_page(message='incorrect password')
             else:
                 models.current_user = linked_author
                 redirect('/')
                 return main_page()
-    else: # request.forms.get("buton") == "Create account"
-        username = request.forms.get("new_username")
-        password1 = request.forms.get("new_password1")
-        password2 = request.forms.get("new_password2")
-        if models.User.objects(username=username).count() != 0:
-            return sign_in_page(message="username taken")
-        elif password1 != password2:
-            return sign_in_page(message="passwords do not match")
+    else:
+
+        # else if request.forms.get("buton") == "Create account"
+
+        if models.User.objects(username=new_username).count() != 0:
+            return sign_in_page(message='username taken')
+        elif new_password1 != new_password2:
+            return sign_in_page(message='passwords do not match')
         else:
-            user = models.User(username=username, password=password1)
+            user = models.User(username=new_username,
+                               password=new_password1)
             user.save()
             models.current_user = user
             redirect('/')
             return main_page()
 
+
 @route('/account')
 def account_page(message=None):
-    if models.current_user==None:
-        return "<b>No user logged in</b>"
+    if models.current_user == None:
+        return '<b>No user logged in</b>'
     else:
-        return template(
-                'templates/account.html',
-                current_user = models.current_user,
-                message = message
-               )
+        return template('templates/account.html',
+                        current_user=models.current_user,
+                        message=message)
+
 
 @post('/account')
 def account_page_action():
-    if request.forms.get("button") == "Update info":
+    if request.forms.get('button') == 'Update info':
+
         # Get fields from page
+
         first_name = request.forms.get('first_name')
         last_name = request.forms.get('last_name')
         email = request.forms.get('email')
@@ -71,73 +81,89 @@ def account_page_action():
         new_pass1 = request.forms.get('new_password1')
         new_pass2 = request.forms.get('new_password2')
 
+        # Set current_user attributes (except password)
+
         cur_user = models.current_user
         cur_user.first_name = first_name
         cur_user.last_name = last_name
         cur_user.email = email
+
+        # Set current_user password attribute
+
         if cur_pass:
             if cur_pass == cur_user.password:
                 if new_pass1 == new_pass2:
                     cur_user.password = new_pass1
                 else:
                     cur_user.save()
-                    return account_page(message="passwords do not match")
+                    return account_page(message='passwords do not match'
+                            )
             else:
                 cur_user.save()
-                return account_page(message="incorrect password")
+                return account_page(message='incorrect password')
         cur_user.save()
-        return account_page(message="success") 
-    else: # else request.forms.get("button") == "Sign out"
+        return account_page(message='success')
+    else:
+
+        # else if request.forms.get("button") == "Sign out"
+
         models.current_user = None
         redirect('/')
         return main_page()
 
+
 @route('/user/<username>')
 def user_page(username):
     if models.User.objects(username=username).count() == 0:
-        return "<b> User '%s' not found</b>"%username
+        return "<b> User '%s' not found</b>" % username
     else:
+
+        # Show posts from user in url
+
         linked_author = models.User.objects(username=username).first()
-        return template(
-                'templates/main.html',
-                current_user = models.current_user,
-                posts=models.Post.objects(author=linked_author),
-                show_post=linked_author==models.current_user,
-                show_comment=False
-           )
+        return template('templates/main.html',
+                        current_user=models.current_user,
+                        posts=models.Post.objects(author=linked_author),
+                        show_post=linked_author == models.current_user,
+                        show_comment=False)
+
 
 @post('/user/<username>')
 def user_page_action(username):
-    post = models.Post(
-                author=models.current_user,
-                title=request.forms.get("post_title"),
-                content=request.forms.get("post_content"),
-                )
+
+    # handle new post
+
+    post = models.Post(author=models.current_user,
+                       title=request.forms.get('post_title'),
+                       content=request.forms.get('post_content'))
     post.save()
     return user_page(username)
 
+
 @route('/post/<username>/<title>')
 def post_page(username, title):
+
+    # show a specific post and its comments
+
     linked_author = models.User.objects(username=username).first()
-    post = models.Post.objects(author=linked_author, title=title).first()
-    return template(
-            'templates/main.html',
-            current_user = models.current_user,
-            posts=[post],
-            show_post=False,
-            show_comment=models.current_user!=None
-           )
+    post = models.Post.objects(author=linked_author,
+                               title=title).first()
+    return template('templates/main.html',
+                    current_user=models.current_user, posts=[post],
+                    show_post=False, show_comment=models.current_user
+                    != None)
+
 
 @post('/post/<username>/<title>')
 def post_page_action(username, title):
+
+    # handle new comment on a post
+
     linked_author = models.User.objects(username=username).first()
-    post = models.Post.objects(author=linked_author, title=title).first()
-    print post.content
-    comment = models.Comment(
-                author = models.current_user,
-                content = request.forms.get("comment_area")
-                )
+    post = models.Post.objects(author=linked_author,
+                               title=title).first()
+    comment = models.Comment(author=models.current_user,
+                             content=request.forms.get('comment_area'))
     post.comments.append(comment)
-    print post.comments
     post.save()
     return post_page(username, title)
